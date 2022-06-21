bfb185fb901e2997135a1058567655f51636de5b
mobiss11 <mobiss11@yandex.ru>
2022-06-21 13:23:40 +0300

    ver 1.0

---
+++ consts.py
@@ -0,0 +1,26 @@
+
+# Table
+FILE_NAME = 'credentials.json'
+PATH_TABLE = 'https://docs.google.com/spreadsheets/d/1NHiuP7YfyQj-O8clL_AzxG3mzcbVz8ldhCsJHmg3GKA/edit#gid=1588464211'
+WORKSHEET = 'Подготовка к выгрузке ВСЕ'
+
+# Woocommerce path
+LOGIN_INPUT = '/html/body/div[1]/form/p[1]/input'
+PASSWORD_INPUT = '/html/body/div[1]/form/div/div/input'
+BUTTON_INPUT = '/html/body/div[1]/form/p[3]/input[1]'
+BUTTON_ADD_PRODUCT = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/a[1]'
+FIELD_NAME = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[1]/div[1]/div[1]/input'
+FIELD_DESCRIPRTION = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[3]/div[1]/div[1]/div[2]/div[3]/div[2]/textarea'
+BUTTON_TAB = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[3]/div[1]/div[1]/div[2]/div[1]/ul/li[4]/a'
+FIELD_CATEGORY = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[3]/div[1]/div[1]/div[2]/div[11]/div[2]/div/input'
+FIELD_LOGO = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[3]/div[1]/div[1]/div[2]/div[12]/div[2]/div/input'
+FIELD_COLOR1 = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[3]/div[1]/div[1]/div[2]/div[9]/div[2]/div/input'
+FIELD_COLOR2 = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[3]/div[1]/div[1]/div[2]/div[10]/div[2]/div/input'
+FIELD_PRICE = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[3]/div[1]/div[2]/div[2]/div/div[1]/div[2]/p[1]/input'
+SEARCH_CATEGORY = 'in-product_cat-176'
+SEARCH_CATEGORY2 = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[2]/div/div[2]/div[2]/div/div[2]/ul/li[7]/ul'
+
+BUTTON_PUBLIC = '/html/body/div[1]/div[2]/div[3]/div[1]/div[5]/form/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div[3]/input[2]'
+
+# Phrases
+FINAL_PHRASE = 'загружен'
---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+path_admin_panel = 'https://stage3.ainsys.a2hosted.com/wp-admin/'
+path_add_product = 'https://stage3.ainsys.a2hosted.com/wp-admin/edit.php?post_type=product'
+login = 'dev10'
+password = 'banan184'
